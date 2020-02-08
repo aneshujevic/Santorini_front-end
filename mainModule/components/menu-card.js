4a2f7657980dd1ae0cf861ba67dd8b89c09571c1
owlhoo <anes1996_h@hotmail.com>
2020-02-09 00:32:44 +0100

    mainModule got renamed :D

--- mainModule/components/menu-card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Card (props) {
-    return (
-        <li className="card"><a href={props.link}>{props.text}</a></li>
-    );
-}
-
-export default Card;
--- mainModule/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Page from './components/index_page.js';
-import './index.css';
-
-ReactDOM.render(
-    <Page />,
-    document.getElementById('root')
-);
